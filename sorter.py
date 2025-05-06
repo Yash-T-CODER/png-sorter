617cb7bbd267f161490fb2bf6c5de1cc2f76e610
Yash-T-CODER <its.yash.goyal24@gmail.com>
2025-05-06 16:33:28 +0530

    updated

--- sorter.py
+++ sorter.py
@@ -1,14 +1,19 @@
 import os
+
 if os.path.exists("sorted folder"):
      pass
 else:
      os.mkdir("sorted folder")
-for i in os.listdir():
-    if ".png" in i:
-        os.rename(i,f"sorted folder/{i}")
-        os.rename(f"sorted folder/{i}",f"sorted folder/{i}")
-print("All file has been moved to a seperate folder")
-print(os.listdir("sorted folder"))
+file_name=input("enter your file type which you want to sort: ")
+if "." in file_name:
+    for i in os.listdir():
+        if file_name in i:
+            os.rename(i,f"sorted folder/{i}")
+            #os.rename(f"sorted folder/{i}",f"sorted folder/{i}{file_name}")
+    print("All file has been moved to a seperate folder")
+    print(os.listdir("sorted folder"))
+else:
+     print("Please enter correct file name")
+
 for i in range(len(os.listdir("sorted folder"))):
-     os.rename(f"sorted folder/{os.listdir("sorted folder")[i]}",f"sorted folder/{i+1}.png")
-               
+     os.rename(f"sorted folder/{os.listdir("sorted folder")[i]}",f"sorted folder/{i+1}{file_name}")
